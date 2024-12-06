41913e724ca8de1a21bc153b00b1b0c6d4228999
Oleg Ponomarev <ponomarevov@gnivc.ru>
2024-12-07 00:00:25 +0300

    Add RestTemplateConnector and its configuration

--- src/main/java/net/osandman/rzdmonitoring/config/WebClientConfiguration.java
+++ src/main/java/net/osandman/rzdmonitoring/config/WebClientConfiguration.java
@@ -7,11 +7,13 @@ import org.eclipse.jetty.client.HttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JettyClientHttpRequestFactory;
 import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.JettyClientHttpConnector;
 import org.springframework.http.codec.ClientCodecConfigurer;
 import org.springframework.http.codec.json.Jackson2JsonDecoder;
 import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -33,6 +35,14 @@ public class WebClientConfiguration {
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             .registerModule(new JavaTimeModule())
             .configure(WRITE_DATES_AS_TIMESTAMPS, false);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        JettyClientHttpRequestFactory jettyClientHttpRequestFactory = new JettyClientHttpRequestFactory();
+        RestTemplate restTemplate = new RestTemplate(jettyClientHttpRequestFactory);
+        restTemplate.getInterceptors().add(new RestTemplateRequestInterceptor());
+        return restTemplate;
     }
 
     @Bean
