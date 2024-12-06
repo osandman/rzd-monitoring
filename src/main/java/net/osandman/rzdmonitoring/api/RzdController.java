ca6614f47042d7d3854b31588126a50712947ced
Oleg Ponomarev <ponomarevov@gnivc.ru>
2024-12-07 00:03:07 +0300

    Add RzdController

---
+++ src/main/java/net/osandman/rzdmonitoring/api/RzdController.java
@@ -0,0 +1,36 @@
+package net.osandman.rzdmonitoring.api;
+
+import lombok.RequiredArgsConstructor;
+import net.osandman.rzdmonitoring.dto.StationDto;
+import net.osandman.rzdmonitoring.dto.TicketsResult;
+import net.osandman.rzdmonitoring.scheduler.TicketsTask;
+import net.osandman.rzdmonitoring.service.StationService;
+import net.osandman.rzdmonitoring.service.TicketService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/rzd")
+@RequiredArgsConstructor
+public class RzdController {
+
+    private final StationService stationService;
+    private final TicketService ticketService;
+
+    @GetMapping("/station")
+    private List<StationDto> findStation(@RequestParam String name) {
+        return stationService.findStations(name);
+    }
+
+    @GetMapping("/tickets")
+    private TicketsResult findTickets(
+        @RequestBody TicketsTask ticketsTask
+    ) {
+        return ticketService.process(ticketsTask);
+    }
+}
