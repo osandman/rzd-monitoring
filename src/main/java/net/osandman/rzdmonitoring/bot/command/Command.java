8a76fd07fe214c0412e404fc457cc294372d90f2
Oleg Ponomarev <ponomarevov@gnivc.ru>
2025-10-30 13:49:27 +0300

    Update bot commands text

--- src/main/java/net/osandman/rzdmonitoring/bot/command/Command.java
+++ src/main/java/net/osandman/rzdmonitoring/bot/command/Command.java
@@ -10,12 +10,12 @@ import java.util.Arrays;
 public enum Command {
 
     START("/start", "\uD83C\uDFAC вывод всех команд", 1), // 🎬
-    TICKETS("/tickets", "\uD83C\uDFAB создание задачи для мониторинга билетов", 2), // 🎫
+    TICKETS("/tickets", "\uD83C\uDFAB создание задачи для отслеживания билетов", 2), // 🎫
     ROUTES("/routes", "\uD83D\uDE9D поиск маршрутов", 3), // 🚝
     STATIONS("/stations", "\uD83C\uDFEB поиск станций", 4), // 🏫
-    TASKS("/tasks", "\uD83D\uDC40 просмотр задач", 5), // 👀
-    UNKNOWN(null, "\uD83D\uDC7D неизвестная команда", 6), // 👽
-    ADM("/adm", "\uD83D\uDC41 админка", 7), // 👁
+    TASKS("/tasks", "\uD83D\uDC40 действия с задачами", 5), // 👀
+    ADM("/adm", "\uD83D\uDC41 админка", 6), // 👁
+    UNKNOWN(null, "\uD83D\uDC7D неизвестная команда", 7), // 👽
     ;
 
     private final String commandStr;
