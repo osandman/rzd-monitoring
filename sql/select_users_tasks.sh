f45b562c28512bbd7b571473e972ef4844ae5265
osandman <o.sandman@gmail.com>
2025-11-16 15:25:23 +0300

    Update select sql script

--- sql/select_users_tasks.sh
+++ sql/select_users_tasks.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-docker exec postgres psql -U osandman -d rzd_db -c "SELECT * FROM rzd_monitoring.users order by created_at desc;"
+docker exec postgres psql -U osandman -d rzd_db -c "SELECT * FROM rzd_monitoring.users order by updated_at desc;"
 
-sudo docker exec postgres psql -U osandman -d rzd_db -c "SELECT t.task_id, t.username as user, t.from_station, \
-t.to_station, t.departure_date as date, ARRAY_TO_STRING(ARRAY(SELECT jsonb_object_keys(train_departure_map)), ', ') as trains, \
+sudo docker exec postgres psql -U osandman -d rzd_db -c "SELECT t.task_id, t.username as user, left(t.from_station, 15) as from, \
+left(t.to_station, 15) as to, t.departure_date as date, ARRAY_TO_STRING(ARRAY(SELECT jsonb_object_keys(train_departure_map)), ', ') as trains, \
 t.seat_filters as filters, t.state, t.execution_count as exec, to_char(t.closed_at, 'DD.MM.YY HH24:MI:SS') as closed \
 FROM rzd_monitoring.tasks t order by t.created_at desc LIMIT 30;"
